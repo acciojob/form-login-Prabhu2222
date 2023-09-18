6f5686c3cff0d1b9f1ff1893acb94a292b3b73d1
Prabhu2222 <120732312+Prabhu2222@users.noreply.github.com>
2023-09-18 21:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getFormvalue() {
 let arr=document.querySelectorAll("input");
 	let firstName=arr[0].value;
 	let lastName=arr[1].value;
-	alert(firstName);
-	alert(lastName)
+	alert(firstName+" "+lastName);
+
 	
 }
