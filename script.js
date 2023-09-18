ad3d44bb470954749d10c62da6a3bc43736474a9
Prabhu2222 <120732312+Prabhu2222@users.noreply.github.com>
2023-09-18 21:49:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+let arr=document.querySelectorAll("input");
+	let firstName=arr[0].value;
+	let lastName=arr[1].value;
+	alert(firstName);
+	alert(lastName)
+	
 }
